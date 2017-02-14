88a5126bb0e39dc2ba29fca60fcb6ca69f3ffa30
cconceicao <conceicaoclaudia5@gmail.com>
2017-02-14 07:51:40 +0000

    Add assets files

---
+++ assets/js/file1.js
@@ -0,0 +1,11 @@
+var text = "Hi, Zecas!"
+
+alert(text)
+
+
+/*
+O eslint retornará os seguintes erros:
+	1:12  error  Strings must use singlequote  quotes
+  1:24  error  Missing semicolon             semi
+  3:12  error  Missing semicolon             semi
+*/
